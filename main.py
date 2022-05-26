112233f4c3a32b3311f6c83f9fada37a35120a3e
Jayne-0 <105009097+Jayne-0@users.noreply.github.com>
2022-05-26 22:28:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
+def find_anagrams(word1, word2):
+# [assignment] Add your code here
+    if(sorted(word1)==sorted(word2)):
+      return True
+    else:
+      return False
+
+# find_anagrams("hello", "ello")
+# find_anagrams("crook", "rocko")
+# find_anagrams("cool", "cool")
+
+print(find_anagrams("hello", "ello")) 
+print(find_anagrams("crook", "rocko")) 
+print(find_anagrams("cool", "cool"))
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
